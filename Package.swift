b1545ce8fb1e091948e224a542b41da34d811062
Adam Fowler <adamfowler71@gmail.com>
2020-06-02 16:58:08 +0100

    Version 4.7.0

--- Package.swift
+++ Package.swift
@@ -1,16 +1,16 @@
 // swift-tools-version:5.0
-
 import PackageDescription
 
 let package = Package(
-  name: "Lambda",
-  products: [
-      .library(name: "Lambda", targets: ["Lambda"]),
-  ],
-  dependencies: [
-      .package(url: "https://github.com/swift-aws/aws-sdk-swift-core.git", .upToNextMinor(from: "4.2.0"))
-  ],
-  targets: [
-      .target(name: "Lambda", dependencies: ["AWSSDKSwiftCore"]),
-  ]
+    name: "Lambda",
+    platforms: [.iOS(.v12), .tvOS(.v12), .watchOS(.v5)],
+    products: [
+        .library(name: "Lambda", targets: ["Lambda"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/swift-aws/aws-sdk-swift-core.git", .upToNextMinor(from: "4.6.0"))
+    ],
+    targets: [
+        .target(name: "Lambda", dependencies: ["AWSSDKSwiftCore"], path: "./Sources/Lambda"),
+    ]
 )
