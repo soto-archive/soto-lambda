852b8960c09882523e152129348d0d7a6db99c92
Adam Fowler <adamfowler71@gmail.com>
2020-09-14 10:56:12 +0100

    Sync with Soto v4.9.0

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .library(name: "Lambda", targets: ["Lambda"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-aws/aws-sdk-swift-core.git", .upToNextMinor(from: "4.6.0"))
+        .package(url: "https://github.com/soto-project/soto-core.git", .upToNextMinor(from: "4.7.0"))
     ],
     targets: [
         .target(name: "Lambda", dependencies: ["AWSSDKSwiftCore"], path: "./Sources/Lambda"),
